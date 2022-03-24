af92586f28e9295af31bbd0514dfcb9d76b681ed
McKotter <93869121+Kalondos@users.noreply.github.com>
2022-03-24 13:01:08 +0200

    Add files via upload

---
+++ pilt.py
@@ -0,0 +1,12 @@
+
+
+
+
+print("\n------------ Vägimees Leiger ------------\n")
+print("Mängu alustamiseks vajutage [Enter] nupule!")
+input()
+
+
+print("Oli kord aeg, mil Hiiumaal ringles ringi üks vägimees.\nKel nimeks olevat Leiger.")
+
+
